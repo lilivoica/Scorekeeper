cc8fa1e0b736e5c6cf2743fc509305f2c85978c9
Lili <lili_voica@yahoo.com>
2018-03-22 11:21:38 +0200

    Second commit

--- app/src/main/java/com/example/lili/scorekeeper/MainActivity.java
+++ app/src/main/java/com/example/lili/scorekeeper/MainActivity.java
@@ -8,6 +8,10 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String SCORE_TEAM_A_BUNDLE_KEY ="scoreTeamA";
+    private final String SCORE_TEAM_B_BUNDLE_KEY ="scoreTeamB" ;
+
     int scoreTeamA = 0;
     int scoreTeamB = 0;
 
@@ -19,70 +23,78 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onSaveInstanceState(Bundle saveInstanceState) {
-        saveInstanceState.putInt("scoreTeamA", scoreTeamA);
-        saveInstanceState.putInt("scoreTeamB", scoreTeamB);
+        saveInstanceState.putInt(SCORE_TEAM_A_BUNDLE_KEY, scoreTeamA);
+        saveInstanceState.putInt(SCORE_TEAM_B_BUNDLE_KEY, scoreTeamB);
+
         super.onSaveInstanceState(saveInstanceState);
     }
 
     @Override
     public void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
-        scoreTeamA = savedInstanceState.getInt("scoreTeamA");
-        scoreTeamB = savedInstanceState.getInt("scoreTeamB");
+        scoreTeamA = savedInstanceState.getInt(SCORE_TEAM_A_BUNDLE_KEY);
+        scoreTeamB = savedInstanceState.getInt(SCORE_TEAM_B_BUNDLE_KEY);
         displayForTeamA(scoreTeamA);
         displayForTeamB(scoreTeamB);
     }
 
-    public void resetScor(View v) {
+    //Resets the score for both teams back to 0.
+    public void resetScore(View v) {
         scoreTeamB = 0;
         scoreTeamA = 0;
         displayForTeamB(scoreTeamB);
         displayForTeamA(scoreTeamA);
     }
 
+    // Displays the given score for Team A/TeamB.
     public void displayForTeamA(int score) {
-        TextView scoreView1 = (TextView) findViewById(R.id.team_a_score);
+        TextView scoreView1 = findViewById(R.id.team_a_score);
         scoreView1.setText(String.valueOf(score));
     }
 
     public void displayForTeamB(int score) {
-        TextView scoreView2 = (TextView) findViewById(R.id.team_b_score);
+        TextView scoreView2 = findViewById(R.id.team_b_score);
         scoreView2.setText(String.valueOf(score));
     }
+    //
 
+    //Increase the score for Team A/B by 1 point.
     public void increaseTeamA(View view) {
         if (scoreTeamA == 100) {
-            Toast.makeText(this, "You cannot have more than 100 points", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, getString(R.string.moreThat100), Toast.LENGTH_SHORT).show();
             return;
         }
-        scoreTeamA = scoreTeamA + 1;
+        scoreTeamA++;
         displayForTeamA(scoreTeamA);
     }
 
     public void increaseTeamB(View view) {
         if (scoreTeamB == 100) {
-            Toast.makeText(this, "You cannot have more than 100 points", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, getString(R.string.moreThat100), Toast.LENGTH_SHORT).show();
             return;
         }
-        scoreTeamB = scoreTeamB + 1;
+        scoreTeamB++;
         displayForTeamB(scoreTeamB);
     }
+    //
 
+    //Decrease the score for Team A/B by 1 point.
     public void decreaseTeamA(View view) {
         if (scoreTeamA == 0) {
-            Toast.makeText(this, "You cannot have less than 0 point", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, getString(R.string.lessThat0), Toast.LENGTH_SHORT).show();
             return;
         }
-        scoreTeamA = scoreTeamA - 1;
+        scoreTeamA--;
         displayForTeamA(scoreTeamA);
     }
 
     public void decreaseTeamB(View view) {
         if (scoreTeamB == 0) {
-            Toast.makeText(this, "You cannot have less than 0 point", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, getString(R.string.lessThat0), Toast.LENGTH_SHORT).show();
             return;
         }
-        scoreTeamB = scoreTeamB - 1;
+        scoreTeamB--;
         displayForTeamB(scoreTeamB);
     }
+    //
 }
